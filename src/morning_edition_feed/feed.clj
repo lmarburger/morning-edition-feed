6bdf69de5807f86de6761347410e712abb260963
Larry Marburger <larry@marburger.cc>
2014-07-24 08:18:14 -0400

    Generate story dates

--- src/morning_edition_feed/feed.clj
+++ src/morning_edition_feed/feed.clj
@@ -1,20 +1,30 @@
 (ns morning-edition-feed.feed
   (:require [net.cgrand.enlive-html :as html])
-  (:use [morning-edition-feed.scrape :only [fetch-latest-stories]]))
+  (:use [morning-edition-feed.scrape :only [fetch-latest-stories]])
+  (:import (java.util Date Locale TimeZone)
+           java.text.SimpleDateFormat))
+
+(defn- ^SimpleDateFormat make-http-format
+  "Formats or parses dates into HTTP date format (RFC 822/1123)."
+  []
+  ;; SimpleDateFormat is not threadsafe, so return a new instance each time
+  (doto (SimpleDateFormat. "EEE, dd MMM yyyy HH:mm:ss ZZZ" Locale/US)
+    (.setTimeZone (TimeZone/getTimeZone "UTC"))))
+
+(defn podcast-date-format [date]
+  (.format (make-http-format) date))
 
 (def ^:dynamic *date-sel* [:rss :> :channel :> :pubDate])
 (def ^:dynamic *story-sel* [:rss :> :channel :> :item])
 
 (html/defsnippet date-model "morning_edition_feed/feed.xml" *date-sel*
   [date]
-  [:pubDate] (html/content date))
+  [:pubDate] (html/content (podcast-date-format date)))
 
 (html/defsnippet story-model "morning_edition_feed/feed.xml" *story-sel*
-  [{id        :id
-    headline  :headline
-    story-url :story-url
-    audio-url :audio-url}]
+  [{:keys [id story-date headline story-url audio-url]}]
   [:guid]      (html/content id)
+  [:pubDate]   (html/content (podcast-date-format story-date))
   [:title]     (html/content headline)
   [:linkx]     (html/content story-url)
   [:enclosure] (html/set-attr :url audio-url))
@@ -25,5 +35,5 @@
   [:item]    (html/substitute (map story-model stories)))
 
 (defn podcast-feed []
-  (let [{date :date stories :stories} (fetch-latest-stories)]
+  (let [{:keys [date stories]} (fetch-latest-stories)]
     (podcast date stories)))
--- src/morning_edition_feed/scrape.clj
+++ src/morning_edition_feed/scrape.clj
@@ -1,41 +1,76 @@
 (ns morning-edition-feed.scrape
   (:require [net.cgrand.enlive-html :as html]
-            [clojure.string :as string]))
+            [clojure.string :as string])
+  (:import (java.util Date Locale TimeZone)
+           java.text.SimpleDateFormat))
+
+;;; TODO: Extract total time
+;;; TODO: Extract episode image
 
 (def ^:dynamic *base-url* "http://www.npr.org/programs/morning-edition/")
 (def ^:dynamic *fake-stories*
   [{:headline "Story 1"
-    :story-url "http://google.com"
     :id "abc123"
-    :audio-url "http://nrp.org/story-1.mp3"}
+    :story-url "http://google.com"
+    :audio-url "http://nrp.org/story-1.mp3"
+    :segment-num "1"}
    {:headline "Second Story"
-    :story-url "http://bing.com"
     :id "def456"
-    :audio-url "http://nrp.org/story-2.mp3"}
+    :story-url "http://bing.com"
+    :audio-url "http://nrp.org/story-2.mp3"
+    :segment-num "2"}
    {:headline "Finale"
-    :story-url "http://yahoo.com"
     :id "ghi789"
-    :audio-url "http://nrp.org/story-3.mp3"}])
+    :story-url "http://yahoo.com"
+    :audio-url "http://nrp.org/story-3.mp3"
+    :segment-num "3"}])
 
-(defn fetch-url [url]
+(defn- ^SimpleDateFormat make-simple-format
+  "Parse simple date strings."
+  []
+  ;; SimpleDateFormat is not threadsafe, so return a new instance each time
+  (doto (SimpleDateFormat. "yyyy-MM-dd" Locale/US)
+    (.setTimeZone (TimeZone/getTimeZone "UTC"))))
+
+;; (defn- ^SimpleDateFormat make-http-format
+;;   "Formats or parses dates into HTTP date format (RFC 822/1123)."
+;;   []
+;;   ;; SimpleDateFormat is not threadsafe, so return a new instance each time
+;;   (doto (SimpleDateFormat. "EEE, dd MMM yyyy HH:mm:ss ZZZ" Locale/US)
+;;     (.setTimeZone (TimeZone/getTimeZone "UTC"))))
+
+(defn parse-simple-date [str]
+  (.parse (make-simple-format) str))
+
+(defn- fetch-url [url]
   (html/html-resource (java.net.URL. url)))
 
-(defn extract-story [node]
-  (let [headline  (html/text (first (html/select [node] [:h1 :a])))
-        id        (get-in (first (html/select [node]
-                                              [[:input (html/attr= :type "hidden")]]))
-                          [:attrs :id])
-        audio-url (get-in (first (html/select [node] [:.download :a]))
-                          [:attrs :href])
-        story-url (get-in (first (html/select [node] [:h1 :a]))
-                          [:attrs :href])]
+(defn- extract-story [date node]
+  (let [headline (-> (html/select [node] [:h1 :a])
+                     first
+                     html/text)
+        id (-> (html/select [node] [[:input (html/attr= :type "hidden")]])
+               first
+               (get-in [:attrs :id]))
+        audio-url (-> (html/select [node] [:.download :a])
+                      first
+                      (get-in [:attrs :href]))
+        story-url (-> (html/select [node] [:h1 :a])
+                      first
+                      (get-in [:attrs :href]))
+        segment-num (-> (html/select [node] [:.segment-num])
+                        first
+                        html/text
+                        Integer.)
+        story-date (-> (.getTime date)
+                       (+ (* segment-num 1000))
+                       Date.)]
     {:id id
      :headline headline
      :story-url story-url
-     :audio-url (string/replace audio-url #"\?dl=1" "")}))
-
-(defn print-story [date story]
-  (println (str " - " date " - " (:headline story))))
+     :audio-url (string/replace audio-url #"\?dl=1" "")
+     :segment-num segment-num
+     :story-date story-date}))
 
 (defn fake-fetch-latest-stories []
   {:date "July 22, 2014"
@@ -43,7 +78,13 @@
 
 (defn fetch-latest-stories []
   (let [raw-body (fetch-url *base-url*)
-        date    (html/text (first (html/select raw-body [:time :span])))
+        date (parse-simple-date
+              (get-in (first (html/select raw-body
+                                          [[:meta (html/attr= :name "date")]]))
+                      [:attrs :content]))
         stories (html/select raw-body
                              [[:.story (complement (html/attr? :id))]])]
-    {:date date :stories (map extract-story stories)}))
+    {:date date :stories (map (partial extract-story date) stories)}))
+
+(defn print-story [date story]
+  (println (str " - " date " - " (:headline story))))
