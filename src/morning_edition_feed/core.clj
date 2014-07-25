5ed57627b4fd5559637b16850bc66543dc0c6a14
Larry Marburger <larry@marburger.cc>
2014-07-24 21:08:28 -0400

    Combine feed and core namespaces

--- src/morning_edition_feed/core.clj
+++ src/morning_edition_feed/core.clj
@@ -1,19 +1,47 @@
 (ns morning-edition-feed.core
-  (:use [morning-edition-feed.feed :only [podcast-feed]]
-        [morning-edition-feed.utils :only [render-to-response]]
+  (:require [net.cgrand.enlive-html :as html])
+  (:use [morning-edition-feed.utils :only [render-to-response]]
+        [morning-edition-feed.scrape :only [fetch-latest-stories]]
         [net.cgrand.moustache :only [app]]
         [environ.core :refer [env]]
         [ring.adapter.jetty :only [run-jetty]]
         [ring.middleware.reload :only [wrap-reload]]
-        [ring.middleware.stacktrace :only [wrap-stacktrace]]))
+        [ring.middleware.stacktrace :only [wrap-stacktrace]])
+  (:import (java.util Date Locale TimeZone)
+           java.text.SimpleDateFormat))
 
 ;;; TODO: Add last-modified response header
 ;;; TODO: Extract total time
 ;;; TODO: Extract episode image
 
-(def routes
-  (app
-   [""] (fn [req] (render-to-response (podcast-feed)))))
+(defn ^SimpleDateFormat make-http-format
+  "Formats or parses dates into HTTP date format (RFC 822/1123)."
+  []
+  ;; SimpleDateFormat is not threadsafe, so return a new instance each time
+  (doto (SimpleDateFormat. "EEE, dd MMM yyyy HH:mm:ss ZZZ" Locale/US)
+    (.setTimeZone (TimeZone/getTimeZone "America/New_York"))))
+
+(defn podcast-date-format [date]
+  (.format (make-http-format) date))
+
+(def ^:dynamic *story-sel* [:rss :> :channel :> :item])
+
+(html/defsnippet story-model "morning_edition_feed/feed.xml" *story-sel*
+  [{:keys [id story-date headline story-url audio-url]}]
+  [:guid]      (html/content id)
+  [:pubDate]   (html/content (podcast-date-format story-date))
+  [:title]     (html/content headline)
+  [:linkx]     (html/content story-url)
+  [:enclosure] (html/set-attr :url audio-url))
+
+(html/deftemplate podcast "morning_edition_feed/feed.xml"
+  [date stories]
+  [:pubDate] (html/content (podcast-date-format date))
+  [:item]    (html/substitute (map story-model stories)))
+
+(defn podcast-feed []
+  (let [{:keys [date stories]} (fetch-latest-stories)]
+    (podcast date stories)))
 
 (defn run-server* [app & {:keys [port] :or {port (Integer. (or (env :port)
                                                                5000))}}]
@@ -29,6 +57,10 @@
 
 (defmacro run-server [app]
   `(run-server* (var ~app)))
+
+(def routes
+  (app
+   [""] (fn [req] (render-to-response (podcast-feed)))))
 
 (defn -main [& args]
   (run-server routes))
